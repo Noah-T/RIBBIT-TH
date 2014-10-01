9ca1d7bf6b6e95fc2cd77e9334c008197236f0e6
Noah-T <Nteshu@gmail.com>
2014-10-01 18:04:31 -0400

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Noah Teshu on 10/1/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ribbit
+//
+//  Created by Noah Teshu on 10/1/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
