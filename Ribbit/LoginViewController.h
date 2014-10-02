da10803bc33ab1329b86ae0e82479021df16bfd9
Noah-T <Nteshu@gmail.com>
2014-10-01 21:28:43 -0400

    added user signup

---
+++ Ribbit/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Ribbit
+//
+//  Created by Noah Teshu on 10/1/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Ribbit/SignupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignupViewController.h
+//  Ribbit
+//
+//  Created by Noah Teshu on 10/1/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignupViewController : UIViewController
+
+@end
