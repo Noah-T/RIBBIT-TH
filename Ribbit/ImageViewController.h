d0f53ccbcff1dd217be64ab47808dec81b177295
Noah-T <Nteshu@gmail.com>
2014-10-06 00:54:19 -0400

    added time limit for viewing photos

---
+++ Ribbit/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  Ribbit
+//
+//  Created by Noah Teshu on 10/5/14.
+//  Copyright (c) 2014 Noah Teshu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@end
